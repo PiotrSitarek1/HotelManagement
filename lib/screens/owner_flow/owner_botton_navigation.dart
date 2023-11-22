c1e6c6bda9476da1e90d844da9d2304b085e7064
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-11-22 23:52:15 +0100

    add owner bottom navigation bar

---
+++ lib/screens/owner_flow/owner_botton_navigation.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:hotel_manager/screens/owner_flow/current_reservations_screen.dart';
+import 'package:hotel_manager/screens/owner_flow/owner_panel_screen.dart';
+
+class OwnerBottomNavigationView extends StatefulWidget {
+  const OwnerBottomNavigationView({super.key});
+
+  @override
+  _OwnerBottomNavigationView createState() => _OwnerBottomNavigationView();
+}
+
+class _OwnerBottomNavigationView extends State<OwnerBottomNavigationView> {
+  int _selectedIndex = 0;
+
+  void _navigateBottomNavigationBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    const CurrentReservationsScreen(),
+    const OwnerPanelScreen()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/blank_background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: _pages[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomNavigationBar,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.event), label: 'Reservations'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), label: 'Owner Panel'),
+        ],
+      ),
+    );
+  }
+}
