7f626ee382a69a834f0818336dcac9834edd83ff
MartynaKalemba <Martynaakalemba@gmail.com>
2023-12-01 20:17:34 +0100

    Make Elements Switch colors after clicking

--- lib/components/service_widget_highlight.dart
+++ lib/components/service_widget_highlight.dart
@@ -2,40 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hotel_manager/models/service_model.dart';
 
+// ignore: must_be_immutable
 class ServiceWidgetHighlight extends StatefulWidget {
   final Service service;
-  const ServiceWidgetHighlight({required this.service, Key? key})
-      : super(key: key);
+  bool _checked = false;
+  Color _colorContainer = Colors.white.withOpacity(0.9);
+
+  ServiceWidgetHighlight({required this.service, Key? key}) : super(key: key);
 
   @override
   _ServiceWidgetHighlight createState() => _ServiceWidgetHighlight();
 }
 
-//todo Color Change doesn't work Wrrrrrrrr.
-
 class _ServiceWidgetHighlight extends State<ServiceWidgetHighlight> {
   @override
   Widget build(BuildContext context) {
-    bool tapped = false;
-    Color _colorContainer = Colors.white.withOpacity(0.9);
-
     return Container(
       height: 80,
       margin: const EdgeInsets.symmetric(vertical: 1.0),
       child: InkWell(
         onTap: () {
           setState(() {
-            _colorContainer = Colors.amber;
+            widget._checked = !widget._checked;
+            widget._colorContainer =
+                widget._checked ? Colors.green : Colors.white.withOpacity(0.9);
           });
         },
         child: Ink(
           height: 150,
-          color: _colorContainer,
           child: Card(
+            color: widget._colorContainer,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(30.0),
             ),
-            color: _colorContainer,
             child: ListTile(
               title: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
