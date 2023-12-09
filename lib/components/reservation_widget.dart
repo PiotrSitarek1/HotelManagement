4f7945c5aac2ae60f1b53ed844deed839850a8da
MartynaKalemba <Martynaakalemba@gmail.com>
2023-12-09 18:34:05 +0100

    Color Change Implementation

--- lib/components/reservation_widget.dart
+++ lib/components/reservation_widget.dart
@@ -24,97 +24,134 @@ class _ReservationWidgetState extends State<ReservationWidget> {
     }
     DateTime checkInDate = DateTime.parse(widget.reservation.date);
     DateTime checkOutDate = DateTime.parse(widget.reservation.dateEnd);
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(50),
-        child: Stack(
-          alignment: AlignmentDirectional.center,
-          children: [
-            Container(
-              height: 300,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(imageUrl), fit: BoxFit.cover),
+        child: ColorFiltered(
+          colorFilter: widget.reservation.status == 'old'
+              ? ColorFilter.mode(
+                  Colors.grey,
+                  BlendMode.color,
+                )
+              : ColorFilter.mode(Colors.transparent, BlendMode.saturation),
+          child: Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                height: 300,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(imageUrl), fit: BoxFit.cover),
+                ),
               ),
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(50),
-              child: Container(
-                height: 250,
-                width: 350,
-                color: const Color.fromRGBO(16, 7, 51, 70),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Container(
+                  height: 250,
+                  width: 350,
+                  color: const Color.fromRGBO(16, 7, 51, 70),
+                ),
               ),
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(50),
-              child: Container(
-                height: 220,
-                width: 320,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(
-                            widget.reservation.hotelname,
+              ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Container(
+                  height: 220,
+                  width: 320,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              widget.reservation.hotelname,
+                              style: GoogleFonts.roboto(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Text(
+                              widget.reservation.adress,
+                              style: GoogleFonts.roboto(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          ),
+                          Text(
+                            'Contact: ${widget.reservation.services}',
                             style: GoogleFonts.roboto(
-                                fontSize: 16,
+                                fontSize: 14,
                                 color: Colors.white,
                                 fontWeight: FontWeight.bold),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(
-                            widget.reservation.adress,
+                          Text(
+                            'Reservation Time:',
                             style: GoogleFonts.roboto(
-                                fontSize: 16,
+                                fontSize: 14,
                                 color: Colors.white,
-                                fontWeight: FontWeight.normal),
+                                fontWeight: FontWeight.bold),
                           ),
-                        ),
-                        Text(
-                          'Reservation Time:',
-                          style: GoogleFonts.roboto(
+                          Text(
+                            '${DateFormat('yyyy-MM-dd hh:mm').format(checkInDate)}',
+                            style: GoogleFonts.roboto(
                               fontSize: 14,
                               color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          '${DateFormat('yyyy-MM-dd hh:mm').format(checkInDate)}',
-                          style: GoogleFonts.roboto(
-                            fontSize: 14,
-                            color: Colors.white,
+                            ),
                           ),
-                        ),
-                        Text(
-                          '${DateFormat('yyyy-MM-dd hh:mm').format(checkOutDate)}',
-                          style: GoogleFonts.roboto(
-                            fontSize: 14,
-                            color: Colors.white,
+                          Text(
+                            '${DateFormat('yyyy-MM-dd hh:mm').format(checkOutDate)}',
+                            style: GoogleFonts.roboto(
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(40),
-                      child: Container(
-                        height: 200,
-                        width: 150,
-                        child: Image(
-                          image: NetworkImage(imageUrl),
-                          fit: BoxFit.cover,
+                          //To nie działa lol
+                          // Padding(
+                          //   padding: const EdgeInsets.all(8),
+                          //   child: Text(
+                          //     'Room Number : ${widget.reservation.roomNumber}',
+                          //     style: GoogleFonts.roboto(
+                          //         fontSize: 14,
+                          //         color: Colors.white,
+                          //         fontWeight: FontWeight.bold),
+                          //   ),
+                          // )
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Text(
+                              'Status : ${widget.reservation.status}',
+                              style: GoogleFonts.roboto(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(40),
+                        child: Container(
+                          height: 200,
+                          width: 150,
+                          child: Image(
+                            image: NetworkImage(imageUrl),
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
